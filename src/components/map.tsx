a826a51b2172db7b3d4e9c6ff56f625b2ac6bca1
humbertoham <hamduranhumberto@gmail.com>
2024-10-14 08:57:20 -0600

    new map

--- src/components/map.tsx
+++ src/components/map.tsx
@@ -3,8 +3,7 @@ export default function Map ()
 {
 return(
     <>
-    <iframe className="rounded-3xl mx-auto my-8 px-2 max-w-5xl" src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3605.4305169760637!2d-99.938144724613!3d25.356883977608362!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMjXCsDIxJzI0LjgiTiA5OcKwNTYnMDguMSJX!5e0!3m2!1sen!2smx!4v1719002546262!5m2!1sen!2smx" width="100%" height="450" allowFullScreen loading="lazy"></iframe>
-  
+    <iframe className="rounded-3xl mx-auto my-8 px-2 max-w-5xl"  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d450.67869286238187!2d-99.93539100440624!3d25.356916640893413!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8662d57ac966a843%3A0x25c6ff1c52267965!2sHacienda%20El%20Fraile!5e0!3m2!1sen!2smx!4v1728917757855!5m2!1sen!2smx" width="100%" height="450"  allowFullScreen loading="lazy"></iframe>
     
     </>
 )
